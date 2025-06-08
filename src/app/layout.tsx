e255c1d75e0b399769a3046501f9b461a9ea1061
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-06-08 15:15:25 +0300

    feat: added basic project structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.scss";
+import "./styles/globals.scss";
+import "./styles/variables.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const NotFound: NextPage = ({}) => {
+  return <div>Page not found</div>;
+};
+
+export default NotFound;
