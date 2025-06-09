776e6f86a7e1232673425b1573a3e62fc1837e4b
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-06-09 17:58:49 +0300

    feat: footer was changed

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,61 @@
 "use client";
+import Link from "next/link";
 import styles from "./header.module.scss";
+import { usePathname } from "next/navigation";
 const Header = ({}) => {
+  const pathName = usePathname();
+
   return (
     <header className={styles.header}>
-      <div className={styles.header__title}>michael-weaver</div>
+      <Link href="/">
+        <div className={styles.header__title}>NX</div>
+      </Link>
       <div className={styles.header__nav_bar}>
         <nav className={styles.header__nav}>
-          <div className={styles.header__nav_btn}>_hello</div>
-          <div className={styles.header__nav_btn}>_about-me</div>
-          <div className={styles.header__nav_btn}>_projects</div>
+          <Link href="/">
+            <div
+              className={styles.header__nav_btn}
+              style={{
+                borderBottom: pathName === "/" ? "2px solid #FFDA61" : "none",
+              }}
+            >
+              _hello
+            </div>
+          </Link>
+          <Link href="/about">
+            <div
+              className={styles.header__nav_btn}
+              style={{
+                borderBottom:
+                  pathName === "/about" ? "2px solid #FFDA61" : "none",
+              }}
+            >
+              _about-me
+            </div>
+          </Link>
+          <Link href="/projects">
+            <div
+              className={styles.header__nav_btn}
+              style={{
+                borderBottom:
+                  pathName === "/projects" ? "2px solid #FFDA61" : "none",
+              }}
+            >
+              _projects
+            </div>
+          </Link>
         </nav>
-        <div className={styles.header__contact}>_contact-me</div>
+        <Link href="/contacts">
+          <div
+            className={styles.header__contact}
+            style={{
+              borderBottom:
+                pathName === "/contacts" ? "2px solid #FFDA61" : "none",
+            }}
+          >
+            _contact-me
+          </div>
+        </Link>
       </div>
     </header>
   );
