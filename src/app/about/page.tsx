a0fed95f0aedba6034cb2999ad2648b83f3935c6
Egor Kutsiy <kycuj.egor2020@gmail.com>
2025-06-18 19:30:21 +0300

    feat: added some styles to dropdown

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
 import { NextPage } from "next";
 import styles from "./page.module.scss";
+import Dropdown from "../../components/dropdown/dropdown";
 
 const AboutPage: NextPage = ({}) => {
   return (
     <div className={styles.about__wrapper}>
       <div className={styles.about__menu}>
         <div className={styles.about__pages}></div>
-        <div className={styles.about__accordion}></div>
+        <div className={styles.about__accordion}>
+          <Dropdown title="personal-info">
+            <div>Adfdfd</div>
+            <div>Bfdfdfdfd</div>
+          </Dropdown>
+        </div>
       </div>
     </div>
   );
--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -2,12 +2,14 @@
 
 import { ReactNode } from "react";
 import styles from "./dropdown.module.scss";
+import { MdArrowRight, MdKeyboardArrowRight } from "react-icons/md";
 
 interface Props {
   title: string;
   width?: string;
   height?: string;
   type?: "heavy" | "light";
+  childGap?: string;
   children: ReactNode | ReactNode[];
 }
 
@@ -16,19 +18,38 @@ const Dropdown = ({
   height = "40px",
   title,
   children,
+  childGap = "8px",
+  type = "heavy",
 }: Props) => {
   return (
     <div className={styles.dropdown__container} style={{ width }}>
-      <div className={styles.dropdown__title} style={{ height }}>
-        {title}
+      <div
+        className={styles.dropdown__title}
+        style={{
+          height,
+          borderBottom: type === "light" ? "none" : "var(--glass-border)",
+        }}
+      >
+        <span className={styles.dropdown__title_into}>
+          {type === "heavy" ? (
+            <MdArrowRight className={styles.dropdown__title_sign} />
+          ) : (
+            <MdKeyboardArrowRight className={styles.dropdown__title_sign} />
+          )}
+          {title}
+        </span>
       </div>
-      {children !== null && !Array.isArray(children) ? (
-        <div>{children}</div>
-      ) : (
-        children?.map((element: ReactNode, index) => (
-          <div key={index}>{element}</div>
-        ))
-      )}
+      <div className={styles.dropdown__elements} style={{ gap: childGap }}>
+        {children !== null && !Array.isArray(children) ? (
+          <div className={styles.dropdown__element}>{children}</div>
+        ) : (
+          children?.map((element: ReactNode, index) => (
+            <div key={index} className={styles.dropdown__element}>
+              {element}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
